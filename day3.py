85ab29ba724ecd56ac6c7b8f96dd7f8fd03fbf0f
dragan.stepanovic <dragan.stepanovic@nultien.rs>
2017-12-03 23:01:49 +0100

    first day second challenge solved

---
+++ day3.py
@@ -0,0 +1,11 @@
+import unittest
+
+class SpiralMemoryTests(unittest.TestCase):
+    def test(self):
+        result = 312051**(1/2)
+        print(result)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
